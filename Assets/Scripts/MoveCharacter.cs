282afa792cc228756ca395e941bb8c730e4ab35d
MuhdSyafeeq <fauzisyafeeq@gmail.com>
2020-12-08 12:18:06 +0800

    Added Player Movement
    - Jump with (Spacebar)
    - Move (WASD)
    - Camera Rotation (Camera Rotate Around Players) [Not fully working]

---
+++ Assets/Scripts/MoveCharacter.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCharacter : MonoBehaviour
+{
+    [SerializeField] CharacterController controller;
+    [SerializeField] float gravity = -9.87f;
+    [SerializeField] float speed = 5.5f;
+    [SerializeField] float jumpHeight = 3f;
+
+    [SerializeField] Transform groundCheck;
+    [SerializeField] float groundDistance = 0.4f;
+    [SerializeField] LayerMask groundMask;
+
+    [SerializeField] private Vector3 velocity;
+    [SerializeField] bool isGrounded;
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0) velocity.y = -2f;
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 movement = (transform.right * x) + (transform.forward * z);
+
+        controller.Move(movement * speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounded) velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    public float turnSpeed = 4.0f;
+    public Transform player;
+
+    private Vector3 offset;
+
+    void Start()
+    {
+        offset = new Vector3(player.position.x, player.position.y + 0.5f, player.position.z - 3.5f);
+    }
+
+    void LateUpdate()
+    {
+        offset = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * turnSpeed, Vector3.up) * offset;
+        transform.position = player.position + offset;
+        transform.LookAt(player.position);
+    }
+
+}
