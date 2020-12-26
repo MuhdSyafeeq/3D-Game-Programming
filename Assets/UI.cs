c6f2fd07498ed725698f9e7395b77bf625d70ba2
MuhdSyafeeq <fauzisyafeeq@gmail.com>
2020-12-26 22:30:08 +0800

    User Interfaces 0.0.1
    Added Visible UI for Players

---
+++ Assets/UI.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class UI : MonoBehaviour
+{
+    [SerializeField] private GameObject[] Outer;
+
+    [SerializeField] Renderer InnerMaterial, OuterMaterial;
+
+    void Update()
+    {
+        if( Input.GetKeyDown(KeyCode.W))
+        {
+            Outer[0].GetComponent<Renderer>().material = InnerMaterial.material;
+            Debug.Log($"In -> {KeyCode.W}");
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Outer[1].GetComponent<Renderer>().material = InnerMaterial.material;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Outer[2].GetComponent<Renderer>().material = InnerMaterial.material;
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            Outer[3].GetComponent<Renderer>().material = InnerMaterial.material;
+        }
+
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            Outer[0].GetComponent<Renderer>().material = OuterMaterial.material;
+            Debug.Log($"Out -> {KeyCode.W}");
+        }
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            Outer[1].GetComponent<Renderer>().material = OuterMaterial.material;
+        }
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            Outer[2].GetComponent<Renderer>().material = OuterMaterial.material;
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            Outer[3].GetComponent<Renderer>().material = OuterMaterial.material;
+        }
+    }
+}
