d261d1f1476c102f08d9e91277ddffeb33ddcf71
yingying0912 <xiiaoying5201314@gmail.com>
2021-01-20 02:13:15 +0800

    Add Cook Scene
    + Cooking script

---
+++ Assets/Scripts/Cook.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Cook : MonoBehaviour
+{
+    string itemName;
+    GameObject item;
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Cooking()
+    {
+        itemName = GetComponentInChildren<Image>().sprite.name;
+        item = Instantiate(GameObject.Find(itemName), new Vector3(0.9f, 0, 0), Quaternion.identity);
+    }
+}
