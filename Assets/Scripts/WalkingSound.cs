67ea01b38b00f185e00692b5e95eb5f9d2ca88ef
yingying0912 <xiiaoying5201314@gmail.com>
2021-02-23 03:07:02 +0800

    UI + Audio
    + Shop GUI
    + Footsteps sound effect

---
+++ Assets/Scripts/WalkingSound.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkingSound : MonoBehaviour
+{
+    [SerializeField] AudioSource walk;
+    private void OnTriggerEnter(Collider other)
+    {
+        walk.Play();
+    }
+}
