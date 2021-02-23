f327925ea897d17168ca53f71a60c1467c87c475
yingying0912 <xiiaoying5201314@gmail.com>
2021-02-24 03:10:06 +0800

    Audio & NPC interaction
    + BGM
    + NPCs
    + shop interior

---
+++ Assets/Scripts/NPCInteraction.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCInteraction : MonoBehaviour
+{
+    [SerializeField] GameObject interaction;
+
+    bool isInteractable;
+    bool isInteracting;
+
+    private void Awake()
+    {
+        isInteractable = false;
+        isInteracting = false;
+        interaction.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (isInteractable && Input.GetKeyDown(KeyCode.E) && !isInteracting)
+        {
+            interaction.SetActive(true);
+            isInteracting = true;
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            isInteractable = true;
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            isInteractable = false;
+            isInteracting = false;
+            interaction.SetActive(false);
+        }
+    }
+}
