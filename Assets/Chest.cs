f8b56effaee45f42b44ca6a376b6784052c21f12
MuhdSyafeeq <fauzisyafeeq@gmail.com>
2021-02-23 19:42:44 +0800

    Chest Added
    - Added Chest which randomly give player Item or Coins

---
+++ Assets/Chest.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : MonoBehaviour
+{
+    [SerializeField] private bool isNearChest = false, pressOnce = false, openAnim = false;
+    [SerializeField] Transform current_cover;
+    [SerializeField] List<Item> RandomItem;
+
+    [SerializeField] int[] coinLimiter;
+    [SerializeField] Currency _currency;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            isNearChest = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            isNearChest = false;
+            //openAnim = false;
+            //pressOnce = false;
+
+            //current_cover.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+        }
+    }
+
+    private void LateUpdate()
+    {
+        if (openAnim)
+        {
+            if(current_cover.rotation.x <= 0 && current_cover.rotation.x >= -0.5)
+            {
+                //Debug.Log(current_cover.rotation.x);
+                current_cover.Rotate(-(Time.deltaTime * 120), 0, 0);
+            }
+        }
+
+        if(Input.GetKeyDown(KeyCode.E) && !(Input.GetKeyUp(KeyCode.E)) && isNearChest)
+        {
+            if (!pressOnce)
+            {
+                pressOnce = true;
+                openAnim = true;
+
+                getItem();
+            }
+        }
+    }
+
+    public void getItem()
+    {
+        int numArr = Random.Range(0, 8);
+        Debug.Log(RandomItem[numArr].name);
+        if (RandomItem[numArr].name == "Coin")
+        {
+            int randomCoin = Random.Range(0, 3);
+            _currency.addCoin(coinLimiter[randomCoin]);
+        }
+        else if(RandomItem[numArr].name != "Coin")
+        {
+            Inventory.instance.inventories.Add(RandomItem[numArr]);
+        }
+    }
+}
--- Assets/Item-Datas/Item.cs
+++ Assets/Item-Datas/Item.cs
@@ -5,10 +5,12 @@ public class Item : ScriptableObject
 {
     new public string name = "New Item";
 
+    [Header("Item Attribute")]
     [SerializeField] public GameObject itemObj;
     [SerializeField] public Sprite icon = null;
     [SerializeField] public bool isIngredient = false;
 
+    [Header("Shop Attribute")]
     [SerializeField] public float buyPrice = 0;
     [SerializeField] public float sellPrice = 0;
 }
